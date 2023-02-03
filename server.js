68bbed59249e676afe73b2df2f1a54f65a2b02bf
Cililocwee <corrie.stroup@gmail.com>
2023-02-04 00:53:55 +0700

    Simple 404 and return link

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+const errorPg = path.join(__dirname, "./404.html");
 
 app.use(express.static("public"));
 
 // Keep last
 app.use((req, res) => {
-  res.status(404).send(`404: Page Not Found ${req.url}`);
+  res.status(404).sendFile(errorPg);
 });
 
 app.listen(port, () => {
